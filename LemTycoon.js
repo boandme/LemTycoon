5674763f4c97f74ad76c5bcb55242ad5fa6a88e8
boandme <vyomj.home@gmail.com>
2024-07-23 11:52:03 -0700

    Selling lemonade & lemonade price(v0.4)

--- LemTycoon.js
+++ LemTycoon.js
@@ -2,8 +2,8 @@ var money = 20;
 var numlemons = 0;
 var numsugars = 0;
 var numlemonades = 0;
-var lemprice = 5;
-document.getElementById("money").innerText = money
+var lemprice = 7;
+lemonade_cost = 7
 function BuyLemon(){
     if (money >= 3) {
         money -= 3
@@ -24,13 +24,35 @@ function BuySugar() {
 
 function makelemonade() {
     if (numlemons >= 2 && numsugars >= 1) {
-        numlemons -= 2
-        numsugars -=1
-        numlemonades += 1
-        document.getElementById("numsugars").innerText = numsugars
-        document.getElementById("numlemons").innerText = numlemons
-        document.getElementById("numlemonades").innerText = numlemonades
+        numlemons -= 2;
+        numsugars -=1;
+        numlemonades += 1;
+        document.getElementById("numsugars").innerText = numsugars;
+        document.getElementById("numlemons").innerText = numlemons;
+        document.getElementById("numlemonades").innerText = numlemonades;
 
+
+    }
+}
+
+function addprice() {;
+    lemprice += 1;
+    document.getElementById("lemprice").innerText = lemprice;
+
+};
+
+function lowerprice() {
+    lemprice -= 1;
+    document.getElementById("lemprice").innerText = lemprice;
+
+};
+setInterval(SellLemonade, 1000)
+function SellLemonade(){
+    if (numlemonades >= 1){
+        numlemonades -= 1
+        money += lemprice
+        document.getElementById("money").innerText = money;
+        document.getElementById("numlemonades").innerText = numlemonades;
 
     }
 }
