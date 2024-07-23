3d06cf11a73fb636bce76a0158831fb27ae71dd2
boandme <vyomj.home@gmail.com>
2024-07-22 22:23:05 -0700

    Make Lemonade Update(v0.2)

--- LemTycoon.js
+++ LemTycoon.js
@@ -1,2 +1,36 @@
 var money = 20;
+var numlemons = 0;
+var numsugars = 0;
+var numlemonades = 0;
+var lemprice = 5;
 document.getElementById("money").innerText = money
+function BuyLemon(){
+    if (money >= 3) {
+        money -= 3
+        numlemons +=1
+        document.getElementById("numlemons").innerText = numlemons
+        document.getElementById("money").innerText = money
+    }
+
+}
+function BuySugar() {
+    if (money >= 1) {
+        money -= 1
+        numsugars +=1
+        document.getElementById("numsugars").innerText = numsugars
+        document.getElementById("money").innerText = money
+    }
+}
+
+function makelemonade() {
+    if (numlemons >= 2 && numsugars >= 1) {
+        numlemons -= 2
+        numsugars -=1
+        numlemonades += 1
+        document.getElementById("numsugars").innerText = numsugars
+        document.getElementById("numlemons").innerText = numlemons
+        document.getElementById("numlemonades").innerText = numlemonades
+
+
+    }
+}
