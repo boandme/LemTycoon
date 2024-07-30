7b8006cf46636de1d6b0c53763303b5edc77fb11
boandme <vyomj.home@gmail.com>
2024-07-30 10:25:04 -0700

    Brownie Update(V0.7)

--- LemTycoon.js
+++ LemTycoon.js
@@ -1,47 +1,48 @@
 // Variables //
-var money = 30;
+var money = 1000;
 var numlemons = 0;
 var numsugars = 0;
 var numstrawberries = 0;
+var numchocolates = 0;
 var numlemonades = 0;
 var numPinkLemonades = 0;
+var numBrownies = 0;
 var lemprice = 8;
 var pinkLemPrice = 12;
 var lemonade_cost = 7;
 var lemrate = 4000;
 var pink_lemrate = 4000;
+var brownie_rate = 4000;
 var Lemonade_min = 9;
 var Lemonade_max = 14;
-var pinkLem_min = 12;
-var pinkLem_max = 17;
+var pinkLem_min = 13;
+var pinkLem_max = 18;
+var brownie_min = 12;
+var brownie_max = 20;
 var current_time = "day"
 var current_day = 1
-// Buy grocery items //
-function BuyLemon(){
-    if (money >= 3) {
-        money -= 3
-        numlemons +=1
-        document.getElementById("numlemons").innerText = numlemons
-        document.getElementById("money").innerText = money
-    }
 
-}
-function BuySugar() {
-    if (money >= 1) {
-        money -= 1
-        numsugars +=1
-        document.getElementById("numsugars").innerText = numsugars
-        document.getElementById("money").innerText = money
-    }
-}
 
-function BuyStrawberry(){
-    if (money >= 4) {
-        money -= 4
-        numstrawberries ++;
-        document.getElementById("numstrawberries").innerText = numstrawberries
+function Buy(item) {
+    if( money >= items[item]) {
+        money -= items[item]
+        if (item === "strawberry") {
+            numstrawberries ++;
+            document.getElementById("numstrawberries").innerText = numstrawberries
+        }
+        else if (item === "lemon") {
+            numlemons ++;
+            document.getElementById("numlemons").innerText = numlemons
+        }
+        else if (item === "sugar") {
+            numsugars ++;
+            document.getElementById("numsugars").innerText = numsugars
+        }
+         else if (item === "chocolate") {
+             numchocolates ++;
+             document.getElementById("numchocolates").innerText = numchocolates
+         }
         document.getElementById("money").innerText = money
-
     }
 }
 
@@ -72,15 +73,30 @@ function makePinkLemonade(){
     }
 }
 
+function makeBrownie() {
+    if (numchocolates >= 1 && numsugars >= 1) {
+        numchocolates -= 1;
+        numsugars -= 1;
+        numBrownies += 1;
+        document.getElementById("numchocolates").innerText = numchocolates;
+        document.getElementById("numsugars").innerText = numsugars;
+        document.getElementById("numBrownies").innerText = numBrownies;
+
+    }
+}
+
 
 
 //Refreshing prices and generating random numbers
 setInterval(refreshprices, 40000)
 function refreshprices(){
-    lemprice = getRandomNumber(Lemonade_min,Lemonade_max)
-    drinks.Pink_Lemonade = getRandomNumber(pinkLem_min,pinkLem_max)
-    document.getElementById("lemprice").innerText = lemprice
-    document.getElementById("pinkLemPrice").innerText = drinks.Pink_Lemonade
+    products.Lemonade = getRandomNumber(Lemonade_min,Lemonade_max)
+    products.Pink_Lemonade = getRandomNumber(pinkLem_min,pinkLem_max)
+    products.Brownie = getRandomNumber(brownie_min, brownie_max)
+    document.getElementById("lemprice").innerText = products.Lemonade
+    document.getElementById("pinkLemPrice").innerText = products.Pink_Lemonade
+    document.getElementById("brownieprice").innerText = products.Brownie
+
 
 }
 function getRandomNumber(min, max) {
@@ -89,17 +105,27 @@ function getRandomNumber(min, max) {
 
   // Sell lemonade auto function //
 
-drinks = {
-    Pink_Lemonade: 12,
+products = {
+    "Pink_Lemonade": 12,
+    "Lemonade": 8,
+    "Brownie": 10,
 }
-console.log(drinks)
+
+items = {
+    "lemon": 3,
+    "sugar": 1,
+    "strawberry":4,
+    "chocolate": 8
+}
+console.log(products)
 setInterval(SellLemonade, lemrate)
 setInterval(SellPinkLemonade, pink_lemrate)
+setInterval(SellBrownie, brownie_rate)
 function SellLemonade(){
     if (numlemonades >= 1){
         numlemonades -= 1
-        money += lemprice
-        console.log(lemprice)
+        money += products.Lemonade
+        console.log(products.Lemonade)
         document.getElementById("money").innerText = money;
         document.getElementById("numlemonades").innerText = numlemonades;
 
@@ -109,10 +135,20 @@ function SellLemonade(){
 function SellPinkLemonade() {
     if(numPinkLemonades >= 1){
         numPinkLemonades -= 1
-        money += drinks.Pink_Lemonade
+        money += products.Pink_Lemonade
         document.getElementById("money").innerText = money;
         document.getElementById("numPinkLemonades").innerText = numPinkLemonades;
 
+    }
+}
+
+function SellBrownie() {
+    if(numBrownies >= 1) {
+        numBrownies -= 1
+        money += products.Brownie
+        document.getElementById("money").innerText = money;
+        document.getElementById("numBrownies").innerText = numBrownies;
+        
     }
 }
 
@@ -122,7 +158,21 @@ function ad1() {
         money -= 100
         Lemonade_min ++;
         Lemonade_max ++;
+        pinkLem_max ++;
+        pinkLem_min ++;
+        brownie_min ++;
+        brownie_max ++;
         document.getElementById("money").innerText = money;
+    }
+}
+
+function ad2() {
+    if ( money >= 500){
+        money -= 500
+        lemrate -= 1000
+        pink_lemrate -= 1000
+        document.getElementById("money").innerText = money;
+        console.log(lemrate)
     }
 }
 
@@ -137,6 +187,23 @@ function pinkLemUpgrade() {
         document.getElementById("buyStraw").style.display= "block";
         money += 10
         document.getElementById("money").innerText = money;
+
+    }
+}
+
+function brownieUpgrade() {
+    if (money >= 350){
+        money -= 350
+        document.getElementById("InvChocolates").style.display = "block";
+        document.getElementById("chocolatier").style.display = "none";
+        document.getElementById("buyChoc").style.display= "block";
+        document.getElementById("brownies").style.display = "block";
+        document.getElementById("makeBrownie").style.display = "block";
+        money += 10
+        document.getElementById("money").innerText = money;
+
+
+
 
     }
 }
